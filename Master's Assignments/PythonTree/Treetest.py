e713d27ad3aa0189b1e90d61c6c30c9a394a3947
Peter Costantino <36424513+pcostjr@users.noreply.github.com>
2025-05-07 14:31:41 -0400

    Added Tree ADT

---
+++ Master's Assignments/PythonTree/Treetest.py
@@ -0,0 +1,9 @@
+from Tree import SimpleDict
+
+if __name__ == '__main__':
+    sd = SimpleDict()
+    sd['key'] = 'a value'
+    print(sd.depth())
+    print('key' in sd)
+    for key in sd:
+        print(key)
