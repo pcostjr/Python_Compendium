97c1360956787fe89c44d43d11a6a9fdde92062a
pcostjr <pcostjr@proton.me>
2025-10-01 10:50:12 -0400

    Screwed around with the sandbox Made some modifications with bubble sort to show efficiency

---
+++ 00_SANDBOX/sandbox.py
@@ -0,0 +1,6 @@
+import random
+choices = ["Cafe", "Crypto Hack", "Battle Royale"]
+
+choice = random.randint(0,2);
+
+print(f"Choice is {choices[choice]}")
--- sorting_algorithms/01_bubble_sort.py
+++ sorting_algorithms/01_bubble_sort.py
@@ -2,7 +2,7 @@
 # description: A simple bubblesort program
 # author: pcostjr
 # created: 09.18.2024
-# last update: 09.18.2024
+# last update: 10.01.2025
 
 # import random library for list generation
 import random
@@ -11,15 +11,22 @@ import random
 values = [random.randint(-100, 100) for i in range(1000)]
 print(f"Initial Values = {values}")
 
+outer_pass = 0
+inner_pass = 0
+
 # perform the bubblesort
 for i in range(len(values) - 1):
+    outer_pass += 1
     # assume the final value in each pass is sorted
-    for j in range(len(values) - i - 1):
+    for j in range(len(values) -i - 1):
+        inner_pass += 1
         # perform the swap using a temp variable
         if values[j] > values[j+1]:
-            temp = values[j]
-            values[j] = values[j+1]
-            values[j+1] = temp
+            values[j], values[j+1] = values[j+1], values[j]
+            # temp = values[j]
+            # values[j] = values[j+1]
+            # values[j+1] = temp
 
 print(f"Sorted Values = {values}")
+print(f"Outer passes: {outer_pass} | Inner passes: {inner_pass}")
 
